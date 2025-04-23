507971b9928a3209fc920138b1ebb69850115001
bencoburn <coburnb1@nku.edu>
2025-04-22 20:44:06 -0400

    Updates

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const organizerRoutes = require('./routes/organizerRoutes');
 const respondentRoutes = require('./routes/respondentRoutes');
 const organizationRoutes = require('./routes/organizationRoutes');
 const attributeRoutes = require('./routes/attributeRoutes');
+const cors = require('cors');
 
 
 dotenv.config();
@@ -13,11 +14,18 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.use(cors()); // Allow all origins
+
+// OR if you want to be specific:
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+
 // Routes
 app.use('/api/organizers', organizerRoutes);
 app.use('/api/respondents', respondentRoutes);
 app.use('/api/orgs', organizationRoutes);
 app.use('/api/orgs', attributeRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5010;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/organizerRoutes.js
+++ routes/organizerRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const organizerController = require('../controllers/organizerController');
 
-router.get('/retrieve', organizerController.retrieve);
+router.post('/retrieve', organizerController.retrieve);
 router.post('/register', organizerController.register);
 router.put('/:id/update', organizerController.update);
 
